ef79ec376f01ec5ebcb6f1989c44ee3b2cd1223f
Sprok Dániel <sprokdaniel@gmail.com>
2023-04-14 21:14:31 +0200

    Firebase authentication 2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.authService.isUserLoggedIn().subscribe(user => {
+      console.log(user);
       this.loggedInUser = user;
     }, error => {
       console.error(error);
